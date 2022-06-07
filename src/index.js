8ebf3a4ab202646591a854bcbe8ec9acbef1bb69
Felipe Lima <70120728+Felipeex@users.noreply.github.com>
2022-06-07 20:59:55 -0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from "react"
+import ReactDOM from "react-dom/client"
+import Home from "./pages/home/index"
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(<Home />)
---
+++ src/pages/home/index.js
@@ -0,0 +1,7 @@
+import "./index.css"
+
+export default function Home() {
+    return (
+        <h1>Hellow Word</h1>
+    )
+}
