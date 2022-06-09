9870c5c5b0e49c0af8cfc05cb25601f5041d3010
Felipe Lima <70120728+Felipeex@users.noreply.github.com>
2022-06-09 19:27:27 -0300

    Fix deploy error (vercel)

--- src/components/routers/private/index.js
+++ src/components/routers/private/index.js
@@ -1,5 +1,5 @@
 import { useContext } from "react";
-import { Route, Navigate } from "react-router-dom";
+import { Navigate } from "react-router-dom";
 import AuthContext from "../../Auth/Context";
 
 const RoutesPrivate = ({ children }) => {
--- src/router.js
+++ src/router.js
@@ -1,7 +1,7 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import AuthProvider from "./components/Auth/Provider";
 import RoutesPrivate from "./components/routers/private";
-import Home from "./pages/Home";
+import Home from "./pages/home";
 import Login from "./pages/Login";
 
 export default function App() {
